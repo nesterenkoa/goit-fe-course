27f72672551e85c87f6e03fc4366a0ef4b12280c
raienko <raienko.a@gmail.com>
2019-04-05 00:03:30 +0300

    add 6 module

---
+++ js/classwork/module-6.js
@@ -0,0 +1,11 @@
+class About {
+constructor (name) {
+  this.name = name;
+}
+}
+let about = new About();
+
+console.log(about);
+console.log(typeof About);
+console.log(typeof about);
+console.log(about instanceof About)
--- js/module-3/home-work-3/home-work.js
+++ js/module-3/home-work-3/home-work.js
@@ -1,31 +1,46 @@
+'use strict';
+
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const isLoginValid = function(login) {
-  if (login.length >= 4 && login.length <= 16){
-    return true
-  } else {
-    return false
-  }
+// const isLoginValid = function(login) {
+//   if (login.length >= 4 && login.length <= 16){
+//     return true
+//   } else {
+//     return false
+//   }
+// };
+
+const  isLoginValid = function (login) {
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
   return !allLogins.includes(login)
 };
 
+const infoInputError = function (login) {
+  console.log(`${login} Ошибка! Логин должен быть от 4 до 16 символов`);
+};
+
+const infoCorrectInput = function (login) {
+  console.log(`${login} Логин успешно добавлен!`)
+};
+
+const infoRelogin = function (login) {
+  console.log(`${login} Такой логин уже используется!`);
+};
+
 const addLogin = function(allLogins, login) {
-  const valid = isLoginValid(login);
-  if (!valid) {
-    return console.log(`${login} Ошибка! Логин должен быть от 4 до 16 символов`);
-  } else {
-    const uniqueness = isLoginUnique(allLogins, login)
-    if (uniqueness) {
-      allLogins.push(login);
-      console.log(`${login} Логин успешно добавлен!`)
-    } else {
-      console.log(`${login} Такой логин уже используется!`);
-    }
-    
+  if (!isLoginValid(login)) {
+    return infoInputError(login)
   }
+  
+  if (!isLoginUnique(allLogins, login)) {
+    return infoRelogin(login)
+  }
+  
+  allLogins.push(login);
+  return infoCorrectInput (login)
 };
 
 addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
